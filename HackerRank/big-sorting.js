e4800182851be1174bdc0b66c0275808a89b9a81
Jaylan Snelson <jaylan210@gmail.com>
2020-06-07 19:45:22 -0400

    Create big-sorting.js

---
+++ HackerRank/big-sorting.js
@@ -0,0 +1,13 @@
+let startArray = [31415926535897932384626433832795, 1, "3", 10, 3, 5];
+
+ function bigSorting(unsorted) {
+    unsorted.sort(function(a,b){
+        if(a.length == b.length){
+            return a > b ? 1 : -1;
+        }
+        return a.length - b.length; 
+    });
+    return unsorted;
+}
+
+bigSorting(startArray);
