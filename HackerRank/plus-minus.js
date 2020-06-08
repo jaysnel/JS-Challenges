802f2555d683e34e93f687fafad0f909db22c084
Jaylan Snelson <jaylan210@gmail.com>
2020-06-08 08:31:27 -0400

    Create plus-minus.js

---
+++ HackerRank/plus-minus.js
@@ -0,0 +1,19 @@
+let arrayList = [-4,3,-9,0,4,1];
+
+function plusMinus(arr) {
+  let arrayLength = arr.length;
+  let pos = 0;
+  let neg = 0;
+  let net = 0;
+  let finalArray = [];
+
+  arr.forEach(el => {  return (el == 0) ? net++ : (el < 0) ? neg++ : pos++ });
+  
+  finalArray.push((pos / arrayLength).toFixed(6));
+  finalArray.push((neg / arrayLength).toFixed(6));
+  finalArray.push((net / arrayLength).toFixed(6));
+
+  console.log(finalArray.join('\n'));
+}
+
+plusMinus(arrayList);
