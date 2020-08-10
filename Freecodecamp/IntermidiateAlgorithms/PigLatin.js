a8af2ea268edbf070b3b92f078778befa416707e
Jaylan Snelson <jaylan210@gmail.com>
2020-08-10 13:18:43 -0400

    Create PigLatin.js

---
+++ Freecodecamp/IntermidiateAlgorithms/PigLatin.js
@@ -0,0 +1,14 @@
+function translatePigLatin(str) {
+
+  let regexValue = /^[^aeiou]+/;
+  let theConstanants = str.match(regexValue);
+
+  if(theConstanants === null) {
+    return `${str}way`;
+  } else {
+    let newStr = str.substr(theConstanants[0].length);
+    return `${newStr}${theConstanants}ay`;
+  }
+}
+
+translatePigLatin("algorithm");
