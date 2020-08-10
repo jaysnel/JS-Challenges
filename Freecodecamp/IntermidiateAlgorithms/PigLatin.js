9aba9ffcd91f306c4f5becefc35c936cd59461be
Jaylan Snelson <jaylan210@gmail.com>
2020-08-10 13:23:48 -0400

    Update PigLatin.js

--- Freecodecamp/IntermidiateAlgorithms/PigLatin.js
+++ Freecodecamp/IntermidiateAlgorithms/PigLatin.js
@@ -3,9 +3,8 @@ function translatePigLatin(str) {
   let regexValue = /^[^aeiou]+/;
   let theConstanants = str.match(regexValue);
 
-  if(theConstanants === null) return `${str}way`;
-  
-  if(theConstanants != null) {
+  if(theConstanants === null) {return `${str}way`}
+  else {
     let newStr = str.substr(theConstanants[0].length);
     return `${newStr}${theConstanants}ay`;
   }
