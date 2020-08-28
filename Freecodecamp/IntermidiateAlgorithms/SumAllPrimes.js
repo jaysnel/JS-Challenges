acbf10b82043ad127c3b81c68c6e015b57c77293
Jaylan Snelson <jaylan210@gmail.com>
2020-08-28 13:13:13 -0400

    Update SumAllPrimes.js

--- Freecodecamp/IntermidiateAlgorithms/SumAllPrimes.js
+++ Freecodecamp/IntermidiateAlgorithms/SumAllPrimes.js
@@ -4,18 +4,18 @@ function sumPrimes(num) {
 
   //push first array which are all values that I need
   for(let i = 1; i <= num; i++) {
-    //check to see if prime number, if it is, push to arry
     if(isPrime(i)) primeArr.push(i);
   }
   //check if isPrime function
   function isPrime(value) {
-    for(let i = 2; i < value; i++) {
-        if(value % i === 0) {
-            return false;
-        }
-    }
-    return value > 1;
-}
+      for(let i = 2; i < value; i++) {
+          if(value % i === 0) {
+              return false;
+          }
+      }
+      return value > 1;
+  }
+
   let final = primeArr.reduce((a, b) => a + b);
   return final;
 }
